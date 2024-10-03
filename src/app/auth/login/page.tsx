19ccf7d9bb3ca30a50bc23fbaccdaf573718f0f0
kevinr2 <kevinreve096@gmail.com>
2024-10-03 13:21:48 -0500

    design

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>login page</h1>
+    </div>
+  );
+}
---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function() {
+  return (
+    <div className="">
+      <h1>New Account</h1>
+    </div>
+  );
+}
