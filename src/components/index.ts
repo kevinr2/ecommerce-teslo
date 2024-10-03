523e482bc2cfa9fff19b81f70d4e5927d607ff99
kevinr2 <kevinreve096@gmail.com>
2024-10-03 17:11:53 -0500

    design part 2

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,16 @@
+
 export * from './products/product-grid/ProductGrid';
 export * from './products/product-grid/ProductGridItem';
 
+
+
+export * from './ui/footer/Footer';
+
+export * from './product/slideshow/ProductMobileSlideshow'
+export * from './product/slideshow/ProductSlideshow'
+
+export * from './product/quantity-selector/QuantitySelector';
+export * from './product/size-selector/SizeSelector';
 
 export * from './ui/not-found/PageNotFound';
 export * from './ui/sidebar/Sidebar';
